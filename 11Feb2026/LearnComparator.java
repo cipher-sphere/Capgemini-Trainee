0901c93737b6ee65ca5d29a1c970b251866e1e30
cipher-sphere <shivam0hapur@gmail.com>
2026-02-11 09:50:17 +0530

    learn comparator

---
+++ 11Feb2026/LearnComparator.java
@@ -0,0 +1,33 @@
+interface Comparator <T>{
+    int compare(T a,T b);
+}
+
+class IntegerCompare implements Comparator<Integer>{
+    public int compare(Integer a,Integer b){
+        return a-b;
+    }
+}
+
+class StringCompare implements Comparator<String>{
+    public int compare(String a,String b){
+        int len = Math.min(a.length(),b.length());
+        for(int i=0;i<len;i++){
+            if(a.charAt(i)!=b.charAt(i)){
+                return a.charAt(i)-b.charAt(i);
+            }
+        }
+        return a.length()-b.length();
+    }
+}
+
+public class LearnComparator {
+public static void main(String[] args){
+    Comparator<Integer> intCmp = new IntegerCompare();
+    System.out.println(intCmp.compare(10, 20));
+    
+    Comparator<String> strCmp = new StringCompare();
+    System.out.println(strCmp.compare("shivam", "shivam"));
+
+}
+    
+}
